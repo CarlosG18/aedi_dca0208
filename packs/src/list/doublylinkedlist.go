78c2b59c72e9e7b398581395e283481382ea8585
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2023-03-22 18:00:10 +0000

    Check erros and add test for doublylinkedlist.go

--- packs/src/list/doublylinkedlist.go
+++ packs/src/list/doublylinkedlist.go
@@ -22,6 +22,7 @@ func (doublylinkedlist *DoublyLinkedList) Init(){
 }
 
 func (doublylinkedlist *DoublyLinkedList) Add(value int){
+  doublylinkedlist.err = ""
   aux := doublylinkedlist.Get_no_nil()
   aux.value = value
   aux.prox = &No1{}
@@ -32,6 +33,7 @@ func (doublylinkedlist *DoublyLinkedList) Add(value int){
 }
 
 func (doublylinkedlist *DoublyLinkedList) Remove(){
+  doublylinkedlist.err = ""
   aux := doublylinkedlist.Get_no_nil()
   aux.ant.prox = nil
   
@@ -40,39 +42,80 @@ func (doublylinkedlist *DoublyLinkedList) Remove(){
 }
 
 func (doublylinkedlist *DoublyLinkedList) RemoveOnIndex(index int){
-  aux := doublylinkedlist.Get_no_cont(index)
-  aux.ant.prox = aux.prox
-
-  doublylinkedlist.tam--
+  doublylinkedlist.err = ""
+  if index == 0{
+    aux := doublylinkedlist.head
+    doublylinkedlist.head = aux.prox
+    aux.prox.ant = nil
+  }else{
+    if index >=0 && index < doublylinkedlist.tam{
+      aux := doublylinkedlist.Get_no_cont(index)
+      aux.ant.prox = aux.prox
+      doublylinkedlist.tam--
+    }else{
+      doublylinkedlist.err = "error: não foi possivel acessar o index!"
+    }
+  }
   doublylinkedlist.Print("RemoveOnIndex")
 }
 
 func (doublylinkedlist *DoublyLinkedList) AddOnIndex(value int, index int){
-  aux := doublylinkedlist.Get_no_cont(index)
+  doublylinkedlist.err = ""
+  if index == 0{
+    aux1 := &No1{}
+    aux := doublylinkedlist.head
+    doublylinkedlist.head = aux1
+    aux1.prox = aux
+    aux.ant = aux1
+    aux1.value = value
+    aux1.ant = nil
+    doublylinkedlist.tam++
+  }else if index > 0 && index < doublylinkedlist.tam{
+    aux := doublylinkedlist.Get_no_cont(index)
+  fmt.Print("index = ", index)
   aux1 := &No1{}
   aux.ant.prox = aux1
   aux1.value = value
   aux1.ant = aux.ant
   aux1.prox = aux
-
+  aux.ant = aux1
   doublylinkedlist.tam++
+  }else{
+    doublylinkedlist.err = "error: não foi possivel acessar o index!"
+  }
   doublylinkedlist.Print("AddOnIndex")
 }
 
 func (doublylinkedlist *DoublyLinkedList) Size() int{
+  doublylinkedlist.err = ""
   doublylinkedlist.Print("Size")
   return doublylinkedlist.tam
 }
 
 func (doublylinkedlist *DoublyLinkedList) Get(index int) int{
-  aux := doublylinkedlist.Get_no_cont(index)
+  aux := doublylinkedlist.head
+  doublylinkedlist.err = ""
+  if index >= 0 && index < doublylinkedlist.tam{
+    aux = doublylinkedlist.Get_no_cont(index)
+  }else{
+    doublylinkedlist.err = "error: não foi possivel acessar o index!"
+    doublylinkedlist.Print("Get")
+    return -1
+  }
+  
   doublylinkedlist.Print("Get")
   return aux.value
 }
  
 func (doublylinkedlist *DoublyLinkedList) Set(value int, index int){
-  aux := doublylinkedlist.Get_no_cont(index)
-  aux.value = value
+  doublylinkedlist.err = ""
+  if index >= 0 && index < doublylinkedlist.tam{
+    aux := doublylinkedlist.Get_no_cont(index)
+    aux.value = value
+  }else{
+    doublylinkedlist.err = "error: não foi possivel acessar o index!"
+  }
+  
   doublylinkedlist.Print("Set")
 }
 
@@ -91,6 +134,7 @@ func (doublylinkedlist *DoublyLinkedList) Print(operation string){
   fmt.Println("Tamanho da doublylinkedlist = ", doublylinkedlist.tam)
   fmt.Println(operation)
   fmt.Println(doublylinkedlist.err)
+  fmt.Println()
 }
 
 func (doublylinkedlist *DoublyLinkedList) Get_no_nil() *No1{
--- packs/tests/list/teste_doublylinkedlist.go
+++ packs/tests/list/teste_doublylinkedlist.go
@@ -13,8 +13,31 @@ func Teste_DoublyLinkedList(){
   for i := 0; i<10; i++{
     dll.Add(i)
   }
-  dll.Remove()
-  dll.RemoveOnIndex(4)
-  dll.AddOnIndex(150,5)
   
+  dll.Remove()
+  
+  dll.RemoveOnIndex(3)
+  dll.RemoveOnIndex(20)
+  dll.RemoveOnIndex(0)
+  dll.RemoveOnIndex(-1)
+  
+  dll.AddOnIndex(150,-1)
+  dll.AddOnIndex(150,0)
+  dll.AddOnIndex(150,3)
+  dll.AddOnIndex(150,30)
+
+  fmt.Println("Size = ", dll.Size())
+  fmt.Println("Get = ", dll.Get(-1))
+  fmt.Println("Get = ", dll.Get(0))
+  fmt.Println("Get = ", dll.Get(3))
+  fmt.Println("Get = ", dll.Get(30))
+
+  dll.Set(30,-1)
+  dll.Set(30,0)
+  dll.Set(30,4)
+  dll.Set(30,80)
+
+  for i := 0; i<5; i++{
+    dll.AddOnIndex(i,i)
+  }
 }
