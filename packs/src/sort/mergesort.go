2944763d8f6280a27b7bd9be73737d26734cf09f
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-04-25 14:12:50 -0300

    add merge

--- packs/src/sort/mergesort.go
+++ packs/src/sort/mergesort.go
@@ -1,5 +1,28 @@
 package sort
 
 func MergeSort(v []int){
+  //função para dividir o vetor v recursivamente
+}
 
+func Merge(v1 []int, v2 []int) []int{
+  n := len(v1)
+  m := len(v2)
+  new_v := make([]int,n+m)
+  cont1 := 0
+  cont2 := 0
+  cont3 := 0
+  
+  for cont3 < len(new_v){
+    if(v1[cont1] > v2[cont2]){
+      new_v[i] = v2[cont2]
+      cont3++
+      cont2++
+    }else{
+      new_v[i] = v1[cont1]
+      cont3++
+      cont1++
+    }
+  }
+  
+  return new_v
 }
