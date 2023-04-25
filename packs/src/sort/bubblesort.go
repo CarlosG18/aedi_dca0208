b46733c749f00bc7fdaeebbea489541fa5a69b26
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-04-25 20:58:50 -0300

    add bubblesort and selectionsort

--- packs/src/sort/bubblesort.go
+++ packs/src/sort/bubblesort.go
@@ -1,5 +1,19 @@
 package sort
 
-func BubbleSort(v []int){
+func BubbleSort(v [7]int) [7]int {
+	tam := len(v)
+	mudou := false
 
+	for j := 0; j < tam-1; j++ {
+		for i := 0; i < tam-j-1; i++ {
+			if v[i] > v[i+1] {
+				v[i], v[i+1] = v[i+1], v[i]
+				mudou = true
+			}
+		}
+		if mudou != true {
+			break
+		}
+	}
+	return v
 }
--- packs/src/sort/selectionsort.go
+++ packs/src/sort/selectionsort.go
@@ -1,5 +1,17 @@
 package sort
 
-func SelectionSort(v []int){
+func SelectionSort(v [7]int) [7]int {
+	tam := len(v)
 
+	for j := 0; j < tam-1; j++ {
+		index_menor := j
+		for i := 1 + j; i < tam; i++ {
+			if v[index_menor] > v[i] {
+				index_menor = i
+			}
+		}
+		v[j], v[index_menor] = v[index_menor], v[j]
+	}
+
+	return v
 }
