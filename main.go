70bb35bae3f7d9fe593c27bb7844d655500f0af8
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-04-26 11:32:37 -0300

    add insertionsort.go complete

--- main.go
+++ main.go
@@ -6,5 +6,9 @@ import (
 )
 
 func main() {
-  
+	v := [5]int{45,29,58,36,1}
+	fmt.Println(v)
+	v1 := sort.InsertionSort(v)
+	fmt.Print(v1)
+
 }
--- packs/src/sort/insertionsort.go
+++ packs/src/sort/insertionsort.go
@@ -4,15 +4,16 @@ func InsertionSort(v [5]int) [5]int {
 	tam := len(v)
 
 	for i := 1; i < tam; i++ {
-		j := i
+		valor := v[i]
 		local := i
+
 		for k := i; k > 0; k-- {
-			if v[j] < v[k-1] {
+			if valor < v[k-1] {
+				v[k] = v[k-1]
 				local = k - 1
-				//colocar uma variavel temporaria para guardar o valor
 			}
 		}
-		v[local] = v[j]
+		v[local] = valor
 	}
 
 	return v
