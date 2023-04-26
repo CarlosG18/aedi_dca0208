6d33b4372749491f6316f583b894dcfa786f8b26
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-04-26 08:36:36 -0300

    init insertionsort.go

--- packs/src/sort/insertionsort.go
+++ packs/src/sort/insertionsort.go
@@ -1,5 +1,19 @@
 package sort
 
-func InsertionSort(v []int){
+func InsertionSort(v [5]int) [5]int {
+	tam := len(v)
 
+	for i := 1; i < tam; i++ {
+		j := i
+		local := i
+		for k := i; k > 0; k-- {
+			if v[j] < v[k-1] {
+				local = k - 1
+				//colocar uma variavel temporaria para guardar o valor
+			}
+		}
+		v[local] = v[j]
+	}
+
+	return v
 }
