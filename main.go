3264b53a749c96a00c31eacfb828c36dc1e07660
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-02 18:59:24 -0300

    Add quicksort.go

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"main/packs/src/sort"
+	//"main/packs/src/sort"
 )
 
 func main() {
-	v := []int{298, 45, 5, 89, 1}
-	fmt.Println(v)
-	v1 := sort.MergeSort(v, len(v))
-	fmt.Print(v1)
 }
--- packs/src/sort/quicksort.go
+++ packs/src/sort/quicksort.go
@@ -1,5 +1,25 @@
 package sort
 
-func QuickSort(v []int){
+func QuickSort(v []int, init int, fin int){
+  if init < fin{
+    pos = Particion(v,init,fin
+    QuickSort(v,init,pos-1)
+    QuickSort(v,pos+1,fin)
+  }
+}
 
+func Particion(v []int, init int, fin int) int{
+  tam := len(v)
+  i := 0
+  pindex := 0
+  pivo := v[fin]
+  
+  for i < tam{
+    if v[i] < pivo{
+      i++
+    }else{
+      pindex++
+      
+    }
+  }
 }
