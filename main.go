44571b87ea3bac13e5d378cbe433bd77bddcd362
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-04 13:44:22 -0300

    add quicksort.go

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
-	//"main/packs/src/sort"
+	//"fmt"
+	"main/packs/src/sort"
 )
 
 func main() {
+  vetor := [7]int{2,7,9,12,0,1,5}
+  sort.QuickSort(vetor,0,len(vetor)-1)
 }
--- packs/src/sort/quicksort.go
+++ packs/src/sort/quicksort.go
@@ -2,24 +2,23 @@ package sort
 
 func QuickSort(v []int, init int, fin int){
   if init < fin{
-    pos = Particion(v,init,fin
+    pos := Particion(v,init,fin)
     QuickSort(v,init,pos-1)
     QuickSort(v,pos+1,fin)
   }
 }
 
 func Particion(v []int, init int, fin int) int{
-  tam := len(v)
-  i := 0
   pindex := 0
   pivo := v[fin]
   
-  for i < tam{
-    if v[i] < pivo{
-      i++
-    }else{
+  for i:=init; i< fin; i++{
+    if v[i] <= pivo{
+      v[i], v[pindex] = v[pindex], v[i]
       pindex++
-      
     }
   }
+  v[pindex], v[fin] = v[fin], v[pindex]
+  
+  return pindex
 }
