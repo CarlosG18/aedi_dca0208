4a73574e3e0972a42c722a70275b739a3eb8919c
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-04 20:44:58 -0300

    compare time of execution of algoritms mergesort and quicksort

--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 	"main/packs/src/sort"
 	"math/rand"
-	//"time"
+	"time"
 )
 
 func main() {
-  vetor := FullArrayRandom(5)
+  vetor := FullArrayRandom(300000)
   fmt.Println("vetor = ", vetor)
-  //start := time.Now()
+  fmt.Println()
+  start := time.Now()
   vetor_ord := sort.MergeSort(vetor, len(vetor))
-  //end := time.Since(start)
+  end := time.Since(start)
   fmt.Println("vetor ordenado com Mergesort = ", vetor_ord)
-  //fmt.Println()
-  //fmt.Println("tempo gasto = ", end)
+  fmt.Println("tempo gasto com MergeSort = ", end)
+  
+  fmt.Println()
+  
+  start1 := time.Now()
+  sort.QuickSort(vetor,0,len(vetor)-1)
+  end1 := time.Since(start1)
+  fmt.Println("vetor ordenado com QuickSort = ", vetor)
+  fmt.Println("tempo gasto com QuickSort = ", end1)
 }
 
 func FullArrayRandom(tam int) []int{
@@ -23,6 +31,15 @@ func FullArrayRandom(tam int) []int{
   
   for i:=0; i<tam; i++{
     vetor[i] = rand.Intn(100)
+  }
+  return vetor
+}
+
+func FullArraySalary(tam int) []int{
+  vetor := make([]int,tam)
+  
+  for i:=0; i<tam; i++{
+    vetor[i] = i
   }
   return vetor
 }
