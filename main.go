85a9a68f671c77f20e1ffa65b03699addd18a621
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-29 12:38:32 -0300

    add size e init remove

--- main.go
+++ main.go
@@ -3,139 +3,156 @@ package main
 import (
 	"fmt"
 	//"main/packs/sort"
-  "main/packs/tree"
+	"main/packs/tree"
 	"math/rand"
 	//"time"
 )
 
 func main() {
-  /* testes algortitmos de ordenação
-  vetor := FullArrayDecres(10000)
-  fmt.Println("vetor = ", vetor)
-  fmt.Println()
-  
-  //BubbleSort
-  start := time.Now()
-  vetor_bubble := sort.BubbleSort(vetor)
-  end := time.Since(start)
-  fmt.Println("vetor ordenado com BubbleSort = ", vetor_bubble)
-  fmt.Println()
-  
-  //SelectionSort
-  start1 := time.Now()
-  vetor_selection := sort.SelectionSort(vetor)
-  end1 := time.Since(start1)
-  fmt.Println("vetor ordenado com SelectionSort = ", vetor_selection)
-  
-  //InsertionSort
-  start2 := time.Now()
-  vetor_insertion := sort.InsertionSort(vetor)
-  end2 := time.Since(start2)
-  fmt.Println("vetor ordenado com InsertionSort = ", vetor_insertion)
-  
-  //MergeSort
-  start3 := time.Now()
-  vetor_merge := sort.MergeSort(vetor,len(vetor))
-  end3 := time.Since(start3)
-  fmt.Println("vetor ordenado com MergeSort = ", vetor_merge)
-  
-  vetor_quick := vetor
-  
-  //QuickSort
-  start4 := time.Now()
-  sort.QuickSort(vetor_quick,0,len(vetor_quick)-1)
-  end4 := time.Since(start4)
-  fmt.Println("vetor ordenado com QuickSort = ", vetor_quick)
-  
-  //CountingSort
-  start5 := time.Now()
-  vetor_couting := sort.CountingSort(vetor)
-  end5 := time.Since(start5)
-  fmt.Println("vetor ordenado com CountingSort = ", vetor_couting)
-  fmt.Println()
-  
-  fmt.Println("tempo gasto com BubbleSort O(n²) = ", end)
-  fmt.Println("tempo gasto com SelectionSort O(n²) = ", end1)
-  fmt.Println("tempo gasto com InsertionSort O(n²) = ", end2)
-  fmt.Println("tempo gasto com MergeSort O(logn) = ", end3)
-  fmt.Println("tempo gasto com QuickSort O(nlogn) = ", end4)
-  fmt.Println("tempo gasto com CountingSort O(n) = ", end5)
-*/
 
-// fazendo testes em arvores
-  vetor := []string{"c","a","r","l","o","s"}
-  t := tree.CreateBst_str(vetor)
-  // bst_verify := t.IsBst()
-  
-  if t != nil{
-    t.PrintPos()
-    // fmt.Println(bst_verify)
-  }else{
-    fmt.Println(t)  
-  }
+	v := []int{10, 7, 3, 8, 15, 20}
+	t := tree.CreateBst(v)
+	u := tree.BstNode{}
+	u.Add(10)
+	u.Add(7)
+	u.Add(3)
+	u.Add(8)
+	u.Add(20)
+	t.Remove(3)
+	t.Remove(20)
+	u.Remove(3)
+	fmt.Println("size da arvore = ", t.Size())
+	t.PrintIn()
+	fmt.Println("size da arvore = ", u.Size())
+	u.PrintIn()
+
+	/* testes algortitmos de ordenação
+	vetor := FullArrayDecres(10000)
+	fmt.Println("vetor = ", vetor)
+	fmt.Println()
+
+	//BubbleSort
+	start := time.Now()
+	vetor_bubble := sort.BubbleSort(vetor)
+	end := time.Since(start)
+	fmt.Println("vetor ordenado com BubbleSort = ", vetor_bubble)
+	fmt.Println()
+
+	//SelectionSort
+	start1 := time.Now()
+	vetor_selection := sort.SelectionSort(vetor)
+	end1 := time.Since(start1)
+	fmt.Println("vetor ordenado com SelectionSort = ", vetor_selection)
+
+	//InsertionSort
+	start2 := time.Now()
+	vetor_insertion := sort.InsertionSort(vetor)
+	end2 := time.Since(start2)
+	fmt.Println("vetor ordenado com InsertionSort = ", vetor_insertion)
+
+	//MergeSort
+	start3 := time.Now()
+	vetor_merge := sort.MergeSort(vetor,len(vetor))
+	end3 := time.Since(start3)
+	fmt.Println("vetor ordenado com MergeSort = ", vetor_merge)
+
+	vetor_quick := vetor
+
+	//QuickSort
+	start4 := time.Now()
+	sort.QuickSort(vetor_quick,0,len(vetor_quick)-1)
+	end4 := time.Since(start4)
+	fmt.Println("vetor ordenado com QuickSort = ", vetor_quick)
+
+	//CountingSort
+	start5 := time.Now()
+	vetor_couting := sort.CountingSort(vetor)
+	end5 := time.Since(start5)
+	fmt.Println("vetor ordenado com CountingSort = ", vetor_couting)
+	fmt.Println()
+
+	fmt.Println("tempo gasto com BubbleSort O(n²) = ", end)
+	fmt.Println("tempo gasto com SelectionSort O(n²) = ", end1)
+	fmt.Println("tempo gasto com InsertionSort O(n²) = ", end2)
+	fmt.Println("tempo gasto com MergeSort O(logn) = ", end3)
+	fmt.Println("tempo gasto com QuickSort O(nlogn) = ", end4)
+	fmt.Println("tempo gasto com CountingSort O(n) = ", end5)
+	*/
+
+	// fazendo testes em arvores
+	// vetor := []string{"c","a","r","l","o","s"}
+	// t := tree.CreateBst_str(vetor)
+	// // bst_verify := t.IsBst()
+
+	// if t != nil{
+	//   t.PrintPos()
+	//   // fmt.Println(bst_verify)
+	// }else{
+	//   fmt.Println(t)
+	// }
 }
 
-func Add_numeros(qtd int, intervalo int){
-  tree := tree.BstNode{}
-  vetor_numeros := FullArrayRandom(qtd,intervalo)
+func Add_numeros(qtd int, intervalo int) {
+	tree := tree.BstNode{}
+	vetor_numeros := FullArrayRandom(qtd, intervalo)
 
-  for i:=0; i<len(vetor_numeros); i++{
-    tree.Add(vetor_numeros[i])
-  }
-  TestTree(&tree, vetor_numeros)
-  fmt.Print("print In = ")
-  tree.PrintIn()
-  fmt.Println()
-  fmt.Print("print Pre = ")
-  tree.PrintPre()
-  fmt.Println()
-  fmt.Print("print Pos = ")
-  tree.PrintPos()
-  fmt.Println()
-  max := tree.Max()
-  min := tree.Min()
-  fmt.Println("max = ", max, " min = ", min)
-  fmt.Println()
-  
+	for i := 0; i < len(vetor_numeros); i++ {
+		tree.Add(vetor_numeros[i])
+	}
+	TestTree(&tree, vetor_numeros)
+	fmt.Print("print In = ")
+	tree.PrintIn()
+	fmt.Println()
+	fmt.Print("print Pre = ")
+	tree.PrintPre()
+	fmt.Println()
+	fmt.Print("print Pos = ")
+	tree.PrintPos()
+	fmt.Println()
+	max := tree.Max()
+	min := tree.Min()
+	fmt.Println("max = ", max, " min = ", min)
+	fmt.Println()
+
 }
 
-func TestTree(tree *tree.BstNode, vetor []int){
-  fmt.Println("testando a busca na árvore = ", vetor)
-  
-  for i:=0; i < 10; i++{
-    busca := tree.Search(i)
-    fmt.Println("procurando o ", i, "achou? ", busca)
-  }
-  fmt.Println()
+func TestTree(tree *tree.BstNode, vetor []int) {
+	fmt.Println("testando a busca na árvore = ", vetor)
+
+	for i := 0; i < 10; i++ {
+		busca := tree.Search(i)
+		fmt.Println("procurando o ", i, "achou? ", busca)
+	}
+	fmt.Println()
 }
 
-func FullArrayRandom(tam int, intervalo int) []int{
-  vetor := make([]int,tam)
-  
-  for i:=0; i<tam; i++{
-    vetor[i] = rand.Intn(intervalo)
-  }
-  return vetor
+func FullArrayRandom(tam int, intervalo int) []int {
+	vetor := make([]int, tam)
+
+	for i := 0; i < tam; i++ {
+		vetor[i] = rand.Intn(intervalo)
+	}
+	return vetor
 }
 
 // func FullArraySemRepet(tam int) []int{
-  
+
 // }
 
-func FullArrayCres(tam int) []int{
-  vetor := make([]int,tam)
-  
-  for i:=0; i<tam; i++{
-    vetor[i] = i
-  }
-  return vetor
+func FullArrayCres(tam int) []int {
+	vetor := make([]int, tam)
+
+	for i := 0; i < tam; i++ {
+		vetor[i] = i
+	}
+	return vetor
 }
 
-func FullArrayDecres(tam int) []int{
-  vetor := make([]int,tam)
-  
-  for i:=0; i<tam; i++{
-    vetor[i] = (tam-1)-i
-  }
-  return vetor
+func FullArrayDecres(tam int) []int {
+	vetor := make([]int, tam)
+
+	for i := 0; i < tam; i++ {
+		vetor[i] = (tam - 1) - i
+	}
+	return vetor
 }
--- packs/tree/bst.go
+++ packs/tree/bst.go
@@ -2,253 +2,281 @@ package tree
 
 import "fmt"
 
-type BstNode struct{
-  left *BstNode
-  value int
-  rigth *BstNode
+type BstNode struct {
+	left  *BstNode
+	value int
+	rigth *BstNode
 }
 
-func NewNode(value int) *BstNode{
-  node := BstNode{}
-  node.left = nil
-  node.rigth = nil
-  node.value = value
-  return &node
+func NewNode(value int) *BstNode {
+	node := BstNode{}
+	node.left = nil
+	node.rigth = nil
+	node.value = value
+	return &node
 }
 
-func (bstnode *BstNode) Add(value int){
-  if value > bstnode.value{
-    if bstnode.rigth != nil{
-      bstnode.rigth.Add(value)
-    }else{
-      bstnode.rigth = NewNode(value)
-    }
-  }else{
-    if bstnode.left != nil{
-      bstnode.left.Add(value)
-    }else{
-      bstnode.left = NewNode(value)
-    }
-  }
+func (bstnode *BstNode) Add(value int) {
+	if value > bstnode.value {
+		if bstnode.rigth != nil {
+			bstnode.rigth.Add(value)
+		} else {
+			bstnode.rigth = NewNode(value)
+		}
+	} else {
+		if bstnode.left != nil {
+			bstnode.left.Add(value)
+		} else {
+			bstnode.left = NewNode(value)
+		}
+	}
 }
 
-func (bstnode *BstNode) Search(value int) bool{
-  if value == bstnode.value{
-    return true
-  }else if value > bstnode.value{
-    if bstnode.rigth == nil{
-      return false
-    }else{
-      return bstnode.rigth.Search(value)
-    }
-  }else{
-    if bstnode.left == nil{
-      return false
-    }else{
-      return bstnode.left.Search(value)
-    }
-  }
+func (bstnode *BstNode) Search(value int) bool {
+	if value == bstnode.value {
+		return true
+	} else if value > bstnode.value {
+		if bstnode.rigth == nil {
+			return false
+		} else {
+			return bstnode.rigth.Search(value)
+		}
+	} else {
+		if bstnode.left == nil {
+			return false
+		} else {
+			return bstnode.left.Search(value)
+		}
+	}
 }
 
 //navegação em arvores
 
-func (bstnode *BstNode) PrintIn(){
-  if bstnode.left != nil{
-    bstnode.left.PrintIn()
-  }
-  fmt.Print(bstnode.value, ", ")
-  if bstnode.rigth != nil{
-    bstnode.rigth.PrintIn()
-  }
+func (bstnode *BstNode) PrintIn() {
+	if bstnode.left != nil {
+		bstnode.left.PrintIn()
+	}
+	fmt.Print(bstnode.value, ", ")
+	if bstnode.rigth != nil {
+		bstnode.rigth.PrintIn()
+	}
 }
 
-func (bstnode *BstNode) PrintPre(){
-  fmt.Print(", ", bstnode.value)
-  if bstnode.left != nil{
-    bstnode.left.PrintPre()
-  }
-  if bstnode.rigth != nil{
-    bstnode.rigth.PrintPre()
-  }
+func (bstnode *BstNode) PrintPre() {
+	fmt.Print(", ", bstnode.value)
+	if bstnode.left != nil {
+		bstnode.left.PrintPre()
+	}
+	if bstnode.rigth != nil {
+		bstnode.rigth.PrintPre()
+	}
 }
 
-func (bstnode *BstNode) PrintPos(){
-  if bstnode.left != nil{
-    bstnode.left.PrintPos()
-  }
-  if bstnode.rigth != nil{
-    bstnode.rigth.PrintPos()
-  }
-  fmt.Print(", ", bstnode.value)
+func (bstnode *BstNode) PrintPos() {
+	if bstnode.left != nil {
+		bstnode.left.PrintPos()
+	}
+	if bstnode.rigth != nil {
+		bstnode.rigth.PrintPos()
+	}
+	fmt.Print(", ", bstnode.value)
 }
 
-func (bstnode *BstNode) Max() int{
-  if bstnode.rigth != nil{
-    return bstnode.rigth.Max()
-  }else{
-    return bstnode.value
-  }
+func (bstnode *BstNode) Max() int {
+	if bstnode.rigth != nil {
+		return bstnode.rigth.Max()
+	} else {
+		return bstnode.value
+	}
 }
 
-func (bstnode *BstNode) Min() int{
-  if bstnode.left != nil{
-    return bstnode.left.Min()
-  }else{
-    return bstnode.value
-  }
+func (bstnode *BstNode) Min() int {
+	if bstnode.left != nil {
+		return bstnode.left.Min()
+	} else {
+		return bstnode.value
+	}
 }
 
-func (bstnode *BstNode) Remove(value int){
-  
+func (bstnode *BstNode) Remove(value int) {
+	if value < bstnode.value {
+		bstnode.left.Remove(value)
+	} else if value > bstnode.value {
+		bstnode.rigth.Remove(value)
+	} else {
+
+		if bstnode.left == nil && bstnode.rigth == nil {
+			fmt.Println(bstnode.value)
+			bstnode = nil
+		}
+		// else if bstnode.rigth != nil && bstnode.left == nil {
+		// 	bstnode = &(*bstnode.rigth)
+		// } else if bstnode.left != nil && bstnode.rigth == nil {
+		// 	bstnode = &(*bstnode.left)
+		// } else {
+
+		// }
+	}
 }
 
-func (bstnode *BstNode) Altura() int{
-  return 1
+func (bstnode *BstNode) Altura() int {
+	return 1
 }
 
-func CreateBst(v []int) *BstNode{
-  repete := IsRepet(v)
-  if repete == false{
-    root := NewNode(v[0])
-    for i:=1; i<len(v); i++{
-      root.Add(v[i])
-    }
-    return root
-  }else{
-    return nil
-  }
+func (bstnode *BstNode) Size() int {
+	size := 1
+
+	if bstnode.left != nil {
+		size += bstnode.left.Size()
+	}
+	if bstnode.rigth != nil {
+		size += bstnode.rigth.Size()
+	}
+	return size
 }
 
-func IsRepet(v []int) bool{
-  repete := false
-  for i:=0; i<len(v); i++{
-    for j:=i+1; j<len(v)-1; j++{
-      if v[i] == v[j]{
-        repete = true
-        break
-      }
-    }
-  }
-  return repete
+func CreateBst(v []int) *BstNode {
+	repete := IsRepet(v)
+	if repete == false {
+		root := NewNode(v[0])
+		for i := 1; i < len(v); i++ {
+			root.Add(v[i])
+		}
+		return root
+	} else {
+		return nil
+	}
 }
 
-func (bstNode *BstNode) IsBst() bool{
-   value := bstNode.value
-  if bstNode.left != nil{
-    if bstNode.left.value > value{
-      return false
-    }else{
-      bstNode.left.IsBst()
-    }  
-  }
-  if bstNode.rigth != nil{
-    if bstNode.rigth.value < value{
-      return false
-    }else{
-      bstNode.rigth.IsBst()
-    }  
-  }
-  return true
+func IsRepet(v []int) bool {
+	repete := false
+	for i := 0; i < len(v); i++ {
+		for j := i + 1; j < len(v)-1; j++ {
+			if v[i] == v[j] {
+				repete = true
+				break
+			}
+		}
+	}
+	return repete
+}
+
+func (bstNode *BstNode) IsBst() bool {
+	value := bstNode.value
+	if bstNode.left != nil {
+		if bstNode.left.value > value {
+			return false
+		} else {
+			bstNode.left.IsBst()
+		}
+	}
+	if bstNode.rigth != nil {
+		if bstNode.rigth.value < value {
+			return false
+		} else {
+			bstNode.rigth.IsBst()
+		}
+	}
+	return true
 }
 
 // versao string
 
-type BstNode_str struct{
-  left *BstNode_str
-  value string
-  rigth *BstNode_str
-}
+// type BstNode_str struct{
+//   left *BstNode_str
+//   value string
+//   rigth *BstNode_str
+// }
 
-func NewNode_str(value string) *BstNode_str{
-  node := BstNode_str{}
-  node.left = nil
-  node.rigth = nil
-  node.value = value
-  return &node
-}
+// func NewNode_str(value string) *BstNode_str{
+//   node := BstNode_str{}
+//   node.left = nil
+//   node.rigth = nil
+//   node.value = value
+//   return &node
+// }
 
-func (bstnode *BstNode_str) Add(value string){
-  if value > bstnode.value{
-    if bstnode.rigth != nil{
-      bstnode.rigth.Add(value)
-    }else{
-      bstnode.rigth = NewNode_str(value)
-    }
-  }else{
-    if bstnode.left != nil{
-      bstnode.left.Add(value)
-    }else{
-      bstnode.left = NewNode_str(value)
-    }
-  }
-}
+// func (bstnode *BstNode_str) Add(value string){
+//   if value > bstnode.value{
+//     if bstnode.rigth != nil{
+//       bstnode.rigth.Add(value)
+//     }else{
+//       bstnode.rigth = NewNode_str(value)
+//     }
+//   }else{
+//     if bstnode.left != nil{
+//       bstnode.left.Add(value)
+//     }else{
+//       bstnode.left = NewNode_str(value)
+//     }
+//   }
+// }
 
-func (bstnode *BstNode_str) PrintIn(){
-  if bstnode.left != nil{
-    bstnode.left.PrintIn()
-  }
-  fmt.Print(bstnode.value, ", ")
-  if bstnode.rigth != nil{
-    bstnode.rigth.PrintIn()
-  }
-}
+// func (bstnode *BstNode_str) PrintIn(){
+//   if bstnode.left != nil{
+//     bstnode.left.PrintIn()
+//   }
+//   fmt.Print(bstnode.value, ", ")
+//   if bstnode.rigth != nil{
+//     bstnode.rigth.PrintIn()
+//   }
+// }
 
-func (bstnode *BstNode_str) PrintPre(){
-  fmt.Print(", ", bstnode.value)
-  if bstnode.left != nil{
-    bstnode.left.PrintPre()
-  }
-  if bstnode.rigth != nil{
-    bstnode.rigth.PrintPre()
-  }
-}
+// func (bstnode *BstNode_str) PrintPre(){
+//   fmt.Print(", ", bstnode.value)
+//   if bstnode.left != nil{
+//     bstnode.left.PrintPre()
+//   }
+//   if bstnode.rigth != nil{
+//     bstnode.rigth.PrintPre()
+//   }
+// }
 
-func (bstnode *BstNode_str) PrintPos(){
-  if bstnode.left != nil{
-    bstnode.left.PrintPos()
-  }
-  if bstnode.rigth != nil{
-    bstnode.rigth.PrintPos()
-  }
-  fmt.Print(", ", bstnode.value)
-}
+// func (bstnode *BstNode_str) PrintPos(){
+//   if bstnode.left != nil{
+//     bstnode.left.PrintPos()
+//   }
+//   if bstnode.rigth != nil{
+//     bstnode.rigth.PrintPos()
+//   }
+//   fmt.Print(", ", bstnode.value)
+// }
 
-func CreateBst_str(v []string) *BstNode_str{
-  repete := IsRepet_str(v)
-  if repete == false{
-    root := NewNode_str(v[0])
-    for i:=1; i<len(v); i++{
-      root.Add(v[i])
-    }
-    return root
-  }else{
-    return nil
-  }
-}
+// func CreateBst_str(v []string) *BstNode_str{
+//   repete := IsRepet_str(v)
+//   if repete == false{
+//     root := NewNode_str(v[0])
+//     for i:=1; i<len(v); i++{
+//       root.Add(v[i])
+//     }
+//     return root
+//   }else{
+//     return nil
+//   }
+// }
 
-func IsRepet_str(v []string) bool{
-  repete := false
-  for i:=0; i<len(v); i++{
-    for j:=i+1; j<len(v)-1; j++{
-      if v[i] == v[j]{
-        repete = true
-        break
-      }
-    }
-  }
-  return repete
-}
+// func IsRepet_str(v []string) bool{
+//   repete := false
+//   for i:=0; i<len(v); i++{
+//     for j:=i+1; j<len(v)-1; j++{
+//       if v[i] == v[j]{
+//         repete = true
+//         break
+//       }
+//     }
+//   }
+//   return repete
+// }
 
-func (bstnode *BstNode_str) Search(value string) bool{
-  
-  
-  if value < bstnode.value && bst{
-    bstnode.left.Search(value)
-  }else if value > bstnode.value{
-    bstnode.rigth.Search(value)
-  }else{
-    return true
-  }
+// func (bstnode *BstNode_str) Search(value string) bool{
 
-  return false
-}
+//   if value < bstnode.value && bst{
+//     bstnode.left.Search(value)
+//   }else if value > bstnode.value{
+//     bstnode.rigth.Search(value)
+//   }else{
+//     return true
+//   }
+
+//   return false
+// }
